bc154ec361b3854ab179c2b0cf82ff619591eead
alim-2023 <alim@crystallitedigital.com>
2023-04-19 22:27:54 +0500

    linking

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
-  const exprtSpan = <span className='fw500 font50 color-white font-f t-center font-f mb-4'><span className='color-blue fw700'>Book Writing Experts</span> Is waiting for you to Ask for a <span className='color-blue fw700'>Professional Ghostwriter</span></span>;
+  const exprtSpan = <span className='fw500 font50 color-white font-f t-center font-f mb-4'><span className='color-blue fw700'><Link className='color-blue hover textdocationnone' href="/">Book Writing Experts</Link></span> Is waiting for you to Ask for a <span className='color-blue fw700'><Link className='color-blue hover textdocationnone' href="/">Professional Ghostwriter</Link></span></span>;
 
   const newSpan = <h1 className='font50 fw700 color-blue font-f hero-span'> <span className=''>Your ONE-STOP</span> <Link className='textdocationnone color-blue hover' href="/"> Ghostwriting, Editing And Publishing Service </Link>  <span className=''>Provider!</span> </h1>;
   
