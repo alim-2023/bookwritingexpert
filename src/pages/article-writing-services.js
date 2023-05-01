a87672217d8864937aef83e1b1c078fc9dca8f52
alim-2023 <alim@crystallitedigital.com>
2023-05-01 23:19:09 +0500

    text

---
+++ src/pages/article-writing-services.js
@@ -0,0 +1,315 @@
+import React from 'react'
+import homebannerlogos from '/public/images/bannerimages/homebannerlogos.png'
+import Banner from '../../components/Banner'
+import Head from 'next/head'
+import Partners from '../../components/Partners'
+import Makestoriesnew from '../../components/Makestoriesnew'
+import Lululogos from '../../components/Lululogos'
+import Aspiring from '../../components/Aspiring'
+import Whychoosebook from '../../components/Whychoosebook'
+import efficientarticlewriting from '/public/images/whychoosebooks/efficientarticlewriting.png'
+import Dowecome from '../../components/Dowecome'
+import Customersatisfaction from '../../components/Customersatisfaction'
+import publishmybook from '/public/images/selfpublishingservices/publishmybook.png'
+import typeofbooks from '/public/images/selfpublishingservices/typeofbooks.png'
+import marketyourbook from '/public/images/selfpublishingservices/marketyourbook.jpg'
+import copy from '/public/images/selfpublishingservices/copy.png'
+import Whychoosebookwritingcube from '../../components/Whychoosebookwritingcube'
+import Link from 'next/link'
+import { Container,Row,Col } from 'react-bootstrap'
+import Bookpublishingservices from '../../components/Bookpublishingservicesweoffer'
+import Whybookpublishing from '../../components/Whybookpublishing'
+import styles from '@/styles/Whybookpublishing.module.css'
+import Professionalseo from '../../components/Professionalseo'
+import Stopworrying from '../../components/Stopworrying'
+import Finetoothedbook from '../../components/Finetoothedbook'
+import Ourprocess from '../../components/Ourprocess'
+import mission1 from '/public/images/mission/1.png'
+import mission2 from '/public/images/mission/2.png'
+import mission3 from '/public/images/mission/3.png'
+
+const Articlewritingservices = () => {
+
+
+  const newSpan = <h1 className='font50 fw700 color-blue font-f hero-span'> <span className=''>    <Link className='fw700 textdocationnone color-blue hover' href="/article-writing-services">Magical Article Writing Services  </Link> That Cast A Spell on Your Target Readers!</span>   </h1>;
+
+  const bannertext=[
+    {
+      title: newSpan,
+      pra: 'Words have an enchanting effect on people, which is why we offer professional article writing services to produce captivating articles.',
+      discuss:'LET S DISCUSS',
+      homebannernum:'(213) 289 3888',
+      banlogo:homebannerlogos,
+      banners:'articlewritingservices',
+     
+       
+
+    }
+  ]
+
+
+  const posdata = <span>Let Our Best <Link className='textdocationnone color-blue hover' href='/book-publishing-services'>Amazon Kindle Direct Publishing (KDP)</Link> Service Providers Handle All Your Publishing Troubles!</span>
+
+
+const whybookpublishingdata = [
+
+{
+  title: 'What is article writing services?',
+  text:  'Article writing services are a great way to get high-quality, well-written articles for your website or blog. By outsourcing your writing needs to a professional service, you may rest assured that you’ll be getting content that is both informative and engaging. Additionally, article writing services can help you build up your online presence by increasing your website’s or blog’s search engine ranking.',
+  col6: '6',
+},
+{
+  title: 'How does an article writing service work?',
+  text: 'Most high-quality article writing services work with a team of writers who create the content for their clients. The client will provide the topic or keywords that they want the content to be about, and the writers will create articles based on those topics.',
+  col6: '6',
+},
+{
+title:'What are the benefits of using an article writing service?',
+text:'There are several benefits to using our best article writing services. First, it can save you a lot of time. If you were to write the articles yourself, it would take up a lot of your time that could be used for other things. Second, it can help you get high-quality content. The writers who work for these services are professionals who know how to write good quality articles. Finally, it can help you build up your online presence. By having good quality content on your website or blog, you will attract more visitors, which can lead to more customers or clients.',
+col6: '6',
+},
+{
+title:'How do professional article writing services benefit businesses?',
+text: 'There are several ways that businesses can benefit from using a professional article writing service. First, it can help you save time. Second, it can help you get high-quality content. The writers who work for these services are professionals who know how to write good quality articles. Finally, it can help you build up your online presence.',
+col6: '6',
+},
+
+]
+
+const  geta = <span>Get A Customized Solution By <Link className='color-blue hover textdocationnone' href='/book-publishing-services'> Best Publishing Services </Link></span>
+
+
+const  work = <span>Your work is over after creating a masterpiece. Let us take the lead from here. Our <Link className='colortextgrey textdocationnone hover' href='/book-publishing-services'> Amazon publishing services </Link> include the three crucial areas of the publishing process: Production, Marketing, and Distribution. As the best publishers, our experience lies in publishing many books by professional writers. And our publishing experts have upgraded many authors to published book authors universally. It’s time for you to get famous!</span>
+
+
+const fine = [
+
+  {
+    img: mission1,
+    title:'Outstanding Marketing Campaigns',
+    text: 'Our team has the expertise, experience and know-how to run excellent book marketing campaigns. We are familiar with the best strategies to target the right prospects to sell your books.',
+    class: 'fintop',
+  
+
+  },
+
+  {
+    img: mission2,
+    title:'Outstanding Marketing Campaigns',
+    text:'Our team has the expertise, experience and know-how to run excellent book marketing campaigns. We are familiar with the best strategies to target the right prospects to sell your books.',
+    class: 'fintop',
+    
+
+  },
+
+  {
+    img: mission3,
+    title:'Outstanding Marketing Campaigns',
+    text:'Our team has the expertise, experience and know-how to run excellent book marketing campaigns. We are familiar with the best strategies to target the right prospects to sell your books.',
+    class: 'fintop',
+
+
+  },
+
+
+]
+
+
+  return (
+   <>
+   <Head>
+
+   <meta charSet="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	<meta name="google-site-verification" content="90Ey_qYXNPX6Ubn6vTFe-C1Iq-3uPgP69ZNmSfckJZU" />
+	<meta name="msvalidate.01" content="B2F5CD44F715E2885953E1B75D19ED7B" />
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="pingback" href="https://bookwritingexperts.com/xmlrpc.php" />
+
+	<meta name='robots' content='index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1' />
+
+	
+	<title>Promising Article Writing Services - Book Writing Experts</title>
+	<meta name="description" content="Article Writing Services in which we throw creativity and element of captivity. Call us now to get the top writing services from the experts." />
+	<link rel="canonical" href="https://bookwritingexperts.com/article-writing-services/" />
+	<meta property="og:locale" content="en_US" />
+	<meta property="og:type" content="article" />
+	<meta property="og:title" content="Promising Article Writing Services - Book Writing Experts" />
+	<meta property="og:description" content="Article Writing Services in which we throw creativity and element of captivity. Call us now to get the top writing services from the experts." />
+	<meta property="og:url" content="https://bookwritingexperts.com/article-writing-services/" />
+	<meta property="og:site_name" content="Book Writing" />
+	<meta property="article:modified_time" content="2023-03-31T11:23:30+00:00" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:label1" content="Est. reading time" />
+	<meta name="twitter:data1" content="7 minutes" />
+
+
+
+
+  <link rel="icon" href="/favicon.svg" />
+
+   </Head>
+   
+
+
+   
+   {/* banner components */}
+
+   {bannertext.map((item, i) =>
+        <Banner key={i}
+          title={item.title}
+          text={item.pra}
+          discuss={item.discuss}
+          homebannernum={item.homebannernum}
+          homebanlogos={item.banlogo}
+          bannershome={item.banners}
+       /> 
+       )}
+   
+
+   {/* partners components */}
+
+
+
+{/* Dowecome */}
+<Dowecome
+ title = {geta}
+ text={work}
+Dowecome= "publishmybook"
+
+/>
+
+<Stopworrying 
+ title = 'Stop worrying about publishing books!'
+ subtitle = {posdata}
+ btn1 = 'Get a free Proposal'
+ btn2 = 'Talk to an expert'
+/>
+
+
+
+{/* Professionalseo */}
+<Professionalseo />
+
+
+
+
+{/* Whybookpublishing */}
+<div className={styles.whybookpublishing}>
+  <Container>
+
+  <Row>
+    <h2 className='font48 fw900 color-blue t-center'>High-Quality Article Writing Services To Expand Your Reach And Join The Big Leagues!</h2>
+    <p className='font15 fw500 colortextgrey t-center pb-5'>
+    Expanding your reach with professional article writing services can be a great way to get your business in front of more potential customers. By having engaging and well-written content on your website, you can attract more visitors and keep them engaged with what you have to offer. Professional research article writing services can help you create content that is both informative and engaging, helping you to connect with more potential customers and grow your business.
+                    </p>
+
+  </Row>
+
+    <Row>
+      {whybookpublishingdata.map((item, i) =>
+        <Whybookpublishing  key={i}
+          title={item.title}
+          text={item.text}
+          col6={item.col6}
+        />
+      )}
+    </Row>            
+  </Container>   
+</div>
+
+{/* Lululogos components */}
+<Lululogos/>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* Why Choose Book Writing Cube? */}
+<Whychoosebook
+   title="Efficient Article Writing Process Curated For Optimal Results"
+  text="Book Writing Cube offers professional and custom article writing services to help businesses reach new heights of success! Here's a closer look at our streamlined process:"
+  whychoose= {efficientarticlewriting}
+ />
+
+<div className={`${styles.finetoothedbook} customizedbook`}>
+<Container>
+<Row >
+<h2 className='color-lightgray   font48   t-center font-f mb-2'>Our Approach</h2>  
+
+<p className='font50 font-f fw500 colortextgrey t-center pb-2'>Our Mission Is To Maximize Value For Our Authors With Online <Link className='textdocationnone color-blue hover' href='/book-publishing-services'>  Book Marketing</Link></p>
+
+</Row>
+
+<Row className='gy-5'>
+{ fine.map((item, i) =>
+<Finetoothedbook   key={i}
+title =  {item.title}
+text =  {item.text}
+classtop =  {item.class}
+img1 =  {item.img}
+btn =  {item.btn}
+/>
+)}
+</Row>
+</Container>
+</div>
+
+<Ourprocess 
+title= 'Our Process'
+
+heading1 = 'Manuscript Submission'
+para1 = 'The writer contacts us for submission of their full-length manuscript, while submitting, we discuss our publishing rules and sign the contract.'
+num1 = '01'
+
+heading2 = 'Peers Review'
+para2 = 'Our experienced editors then review the manuscript and make the required changes with the writer’s permission.'
+num2 = '02'
+
+heading3 = 'Formatting and Typesetting'
+para3 = 'After the manuscript is reviewed by editors, our experts do the formatting and add images.'
+num3 = '03'
+
+heading4 = 'Book Production'
+para4 = 'We design the first copy of the book and take reviews from the writer. The rest of the books are produced on the same pattern while keeping all the important aspects into consideration.'
+num4 = '04'
+
+heading5 = 'Marketing, Publishing, and Distribution'
+para5 = 'Before publishing, we spread awareness about the book to create demand. And Voila! The book is published now!'
+num5 = '05'
+
+
+/>
+
+
+{/* Aspiring */}
+<Aspiring
+ title= 'Get High-Quality and Effective Article Writing Services for Affordable Prices at Book Writing Cube!'
+ text="Get in touch with one of our industry experts for more information about our online article writing services and pricing details by leaving your email or phone number below!"
+ number="(213) 289 3888"
+ discuss="LET'S DISCUSS"
+/>
+
+
+
+
+
+
+
+
+   </>
+  )
+}
+
+export default Articlewritingservices
---
+++ src/pages/book-cover-design-services.js
@@ -0,0 +1,294 @@
+import React from 'react'
+import homebannerlogos from '/public/images/bannerimages/homebannerlogos.png'
+import Banner from '../../components/Banner'
+import Head from 'next/head'
+import Makestoriesnew from '../../components/Makestoriesnew'
+import Lululogos from '../../components/Lululogos'
+import Aspiring from '../../components/Aspiring'
+import Whychoosebook from '../../components/Whychoosebook'
+import fivestepprocess from '/public/images/whychoosebooks/fivestepprocess.png'
+import Dowecome from '../../components/Dowecome'
+import Stillonthefence from '../../components/Stillonthefence'
+import Stopworrying from '../../components/Stopworrying'
+import Link from 'next/link'
+import { Container,Row } from 'react-bootstrap'
+import Whybookpublishing from '../../components/Whybookpublishing'
+import styles from '@/styles/Whybookpublishing.module.css'
+
+const Bookcoverdesignservices = () => {
+
+
+  const newSpan = <h1 className='font50 fw700 color-blue font-f hero-span'> <span className=''>    <Link className='fw700 textdocationnone color-blue hover' href="/blog-writing-services">Book Cover Design Services  </Link> To Help Your Manuscript The Best Selling Book Of All Times!</span>   </h1>;
+
+  const bannertext=[
+    {
+      title: newSpan,
+      discuss:'LET S DISCUSS',
+      homebannernum:'(213) 289 3888',
+      banlogo:homebannerlogos,
+      banners:'bookcoverdesignservices',
+     
+       
+
+    }
+  ]
+
+
+
+
+const whybookpublishingdata = [
+
+{
+  title: 'What Makes A Good Book Cover Design?',
+  text:  'A good book cover design is important because it is the first thing that a potential reader will see, so it needs to be eye-catching and attractive enough to make them want to pick up the book and read it. There are a few things that make a good book cover design. First, it does have an easy-to-read title and easy-to-recognize author information prominently displayed. In addition, the book cover should accurately reflect the genre of the book, i.e., a comic book cover creator will design a different cover for comic books than romance novels. Last but not least, it is also important to remember that the book cover is an important marketing tool. Therefore, the cover should accurately reflect the book’s content. More so, a good cover design can also help set the book’s tone and give readers an idea of what to expect from the story.',
+  col6: '6',
+},
+{
+  title: 'What Is Book Cover Design?',
+  text: 'Book cover design is the process of creating an attractive and eye-catching book cover that accurately represents the contents of the book. It is critical to remember that the book cover is often the first thing potential readers will see, so it is important to make a good impression.',
+  col6: '6',
+},
+{
+title:'What Do I Need To Provide To The Book Cover Designer?',
+text:'When you’re ready to start working with a designer on your book cover, you’ll need to provide them with some basic information about your book, including the title, author name, publisher, and publication date. You should also have a good idea of the overall look, the target audience, and the feel that you’re hoping to achieve with the cover. If you have any specific images or other elements that you would like to include, be sure to share those with the designer.',
+col6: '6',
+},
+{
+title:'How Can I Protect My Book Cover Design?',
+text: 'If you want to protect your book cover design, you can register it with the U.S. Copyright Office. You will need to provide a copy of the cover design, along with a description of the work and your contact information. Once your copyright is registered, you will have the legal right to take action if someone unauthorized uses your design.',
+col6: '6',
+},
+
+]
+
+
+
+const reasons = [
+
+  {
+    title:'Seasoned Designers',
+    text:'We have a team of highly skilled and experienced designers who can create stunning book covers that ensure your book stands out from the rest. From offering eBook cover design services to Amazon book cover design services, there is nothing we can’t do when it comes to designing covers for your book.',
+    col:'6',
+    classnum: 'number',
+  },
+  {
+    title:'The Use of the State-of-the-art Tools',
+    text:'We use the latest design software and technologies to create your high-end book covers, ensuring that they are of the highest quality.',
+    col:'6',
+    classnum: 'number',
+  },
+  {
+    title:'Variety to Choose From',
+    text:'We offer a wide range of book cover design services, so you can choose the one that best suits your needs.',
+    col:'6',
+    classnum: 'number',
+  },
+
+  {
+    title:'Fast Turnaround Time',
+    text:'We have a fast turnaround time and can deliver your book cover within days if you need it urgently.',
+    col:'6',
+    classnum: 'number',
+  },
+  {
+    title:'No Compromise on Quality',
+    text:'Our professionals never compromise on quality, so rest assured you will be provided with only the best.',
+    col:'6',
+    classnum: 'number',
+  },
+  {
+    title:'Excellent Customer Service',
+    text:'We are always all ears to your queries and ensure your no questions remain unanswered.',
+    col:'6',
+    classnum: 'number',
+  },
+
+
+]
+
+const  geta = <span>Get A Customized Solution By <Link className='color-blue hover textdocationnone' href='/book-publishing-services'> Best Publishing Services </Link></span>
+
+
+const  work = <span>Your work is over after creating a masterpiece. Let us take the lead from here. Our <Link className='colortextgrey textdocationnone hover' href='/book-publishing-services'> Amazon publishing services </Link> include the three crucial areas of the publishing process: Production, Marketing, and Distribution. As the best publishers, our experience lies in publishing many books by professional writers. And our publishing experts have upgraded many authors to published book authors universally. It’s time for you to get famous!</span>
+
+const posdata = <span>Let Our Best <Link className='textdocationnone color-blue hover' href='/book-publishing-services'>Amazon Kindle Direct Publishing (KDP)</Link> Service Providers Handle All Your Publishing Troubles!</span>
+
+
+  return (
+   <>
+   <Head>
+
+   <meta charSet="UTF-8" />
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+    <title>Book Cover Design Services – Book Writing Cube</title>
+    <meta name="description"
+        content="Book Cover Design Services to help your book make a striking impression and give your readers a glimpse of what your book is all about. Get yours now. " />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="robots" content="max-image-preview:large" />
+    <meta name="tags" content="" />
+  
+    <meta name="DC.title" content="book cover design" />
+    <meta name="geo.region" content="GB" />
+    <meta name="geo.position" content="54.702355;-3.276575" />
+    <meta name="ICBM" content="54.702355, -3.276575" />
+
+    <meta name="DC.title" content="book cover design" />
+    <meta name="geo.region" content="US" />
+    <meta name="geo.position" content="39.78373;-100.445882" />
+    <meta name="ICBM" content="39.78373, -100.445882" />
+    <meta property="og:locale" content="en" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Book Cover Design Services – Book Writing Cube" />
+    <meta property="og:description"
+        content="Book Cover Design Services to help your book make a striking impression and give your readers a glimpse of what your book is all about. Get yours now." />
+    <meta property="og:url" content="https://www.bookwritingcube.com/" />
+    <meta property="og:site_name" content="Book Cover Design Services – Book Writing Cube" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:label1" content="Est. reading time" />
+    <meta name="twitter:data1" content="5 minutes" />
+    <meta name="facebook-domain-verification" content="s4ocl50k325dpk61h3frbd8aajeybv" />
+
+
+
+  <link rel="icon" href="/favicon.svg" />
+
+   </Head>
+   
+
+
+   
+   {/* banner components */}
+
+   {bannertext.map((item, i) =>
+        <Banner key={i}
+          title={item.title}
+          text={item.pra}
+          subtext1={item.subtext1}
+          subtext2={item.subtext2}
+          discuss={item.discuss}
+          homebannernum={item.homebannernum}
+          homebanlogos={item.banlogo}
+          bannershome={item.banners}
+       /> 
+       )}
+   
+
+   {/* partners components */}
+
+
+
+{/* Dowecome */}
+<Dowecome
+ title = {geta}
+ text={work}
+Dowecome= "publishmybook"
+
+/>
+
+
+<Stopworrying 
+ title = 'Stop worrying about publishing books!'
+ subtitle = {posdata}
+ btn1 = 'Get a free Proposal'
+ btn2 = 'Talk to an expert'
+/>
+
+
+{/* Makestories */}
+<Makestoriesnew
+title='Done and Dusted Book Cover Design Projects That Shed Light on Our Credibility'
+para='Here are a few book cover design projects that we accomplished with utmost finesse.'
+/>
+
+
+{/* Whybookpublishing */}
+<div className={styles.whybookpublishing}>
+  <Container>
+
+  <Row>
+    <h2 className='font48 fw900 color-blue t-center'>Don’t Let Your Questions Pester You Any Longer. We Have Answers To Rein Them In!</h2>
+    <p className='font15 fw500 colortextgrey t-center pb-5'>
+    Our book cover design experts go above and beyond to make your book look flawless and attention-grabbing. Whether you are an author who wants design for their books or a business person who wants their publications to look professional, we serve as the perfect stop to pause. Book Writing Cube sets the bar high with its standard book cover design services and knows what its clients may be thinking of. So, here are some frequently asked questions.
+                    </p>
+
+  </Row>
+
+    <Row>
+      {whybookpublishingdata.map((item, i) =>
+        <Whybookpublishing  key={i}
+          title={item.title}
+          text={item.text}
+          col6={item.col6}
+        />
+      )}
+    </Row>            
+  </Container>   
+</div>
+
+{/* Lululogos components */}
+<Lululogos/>
+
+
+
+
+
+{/* Stillonthefence component */}
+
+<Container className='still'>
+
+
+<Row >
+<h2 className='font48 fw900 color-blue t-center font-f mb-5'>What Makes Us The Best Book Cover Design Services Provider In The Whole Country?</h2>  
+</Row>  
+
+<Row className='gy-5'>
+{ reasons.map((item, i) =>
+<Stillonthefence key={i}
+title =  {item.title}
+text =   {item.text}
+col6 = { item.col }
+classnumber = { item.classnum }
+/>
+  )}
+
+</Row>  
+</Container>
+
+
+
+
+
+
+
+
+
+
+
+
+{/* Why Choose Book Writing Cube? */}
+<Whychoosebook
+   title="Five-Step Process for Book Cover Design Services."
+  whychoose= {fivestepprocess}
+ />
+
+
+{/* Aspiring */}
+<Aspiring
+ title= 'Professional Book Cover Design Services That Help Your Ideal Readers Judge Your Book By Its Cover!'
+ text="Call us if you want to get your hands on the beautiful, engaging, and effective book cover to break the competition and make a 'NAME.' "
+ number="(213) 289 3888"
+ discuss="LET'S DISCUSS"
+/>
+
+
+
+
+
+
+
+
+   </>
+  )
+}
+
+export default Bookcoverdesignservices
