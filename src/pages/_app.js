943af3aed8b60c5b7e8703a796647debbe7e75b6
alim-2023 <alim@crystallitedigital.com>
2023-05-18 00:09:15 +0500

    add sitemao

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,24 +1,23 @@
 import '@/styles/globals.css'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
-// const ZENDESK_KEY = "d1d4aa8a-5573-473a-b921-5b6a8eb467ad";
-// import Zendesk, { ZendeskAPI } from "../pages/ZendexConfig";
+const ZENDESK_KEY = "d1d4aa8a-5573-473a-b921-5b6a8eb467ad";
+import Zendesk, { ZendeskAPI } from "../pages/ZendexConfig";
 
 export default function App({ Component, pageProps }) {
 
-// const handleLoaded = () => {
-//     // ZendeskAPI("messenger", "open");
-//     zE('webWidget:on', 'open', function() {
-//       console.log('The widget has been opened!');
-//     });
-//   };
+const handleLoaded = () => {
+    // ZendeskAPI("messenger", "open");
+    zE('webWidget:on', 'open', function() {
+    });
+  };
 
-//   function openZendeskChat(e) {
-//     e.preventDefault();
-//     if (typeof zE !== 'undefined') {
-//       zE('webWidget', 'open')
-//     }
-//   }
+  // function openZendeskChat(e) {
+  //   e.preventDefault();
+  //   if (typeof zE !== 'undefined') {
+  //     zE('webWidget', 'open')
+  //   }
+  // }
 
   return(
   <>
@@ -26,10 +25,10 @@ export default function App({ Component, pageProps }) {
   <Component {...pageProps} />
   <Footer />
   
-   {/* <div>
+   <div>
       <Zendesk defer zendeskKey={ZENDESK_KEY} onLoaded={handleLoaded} />
-      <div href="#" onClick={openZendeskChat}>Open Zendesk Chat</div>
-    </div> */}
+      {/* <div href="#" onClick={openZendeskChat}>Open Zendesk Chat</div> */}
+    </div>
 
 
   </>
