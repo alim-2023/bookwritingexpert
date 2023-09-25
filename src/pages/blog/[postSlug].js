b6e7e37bcf068e4a5d43084a58132895a81f01a4
cl-farooqaziz <dev.faz@production-dev.com>
2023-09-25 11:34:48 -0700

    error

--- src/pages/blog/[postSlug].js
+++ src/pages/blog/[postSlug].js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Link from 'next/link'
 import Image from "next/image";
+import { useState } from 'react';
 import styles from '@/styles/InnerBlog.module.css'
 import { Container, Row, Col } from 'react-bootstrap'
 //
